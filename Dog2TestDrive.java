06fb0eae006615d63b2420057bf3d610118a6c41
Alexandr <alexandrzvarygin@gmail.com>
2017-11-16 22:27:43 +0300

    rabota s obektami Dog2TestDrive

---
+++ Dog2TestDrive.java
@@ -0,0 +1,14 @@
+class Dog2TestDrive {
+	public static void main (String[] args) {
+	Dog2 one = new Dog2();
+	one.size  = 15;
+	Dog2 two = new Dog2();
+	two.size = 70;
+	Dog2 three = new Dog2();
+	three.size = 5;
+
+	one.bark();
+	two.bark();
+	three.bark(); 
+	}
+}
