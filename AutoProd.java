564a6c23efce82a626c4ca226be3a253b5c49890
alexweste <alexandrzvarygin@gmail.com>
2018-02-04 22:02:15 +0300

    Creating file Auto Prodvijenie

---
+++ AutoProd.java
@@ -0,0 +1,13 @@
+class AutoProd {
+	public static void main (String args[]){
+	byte a = 70;
+	byte b = 80;
+	byte c = 100;
+	int d = a * b / c;
+	System.out.println("Исходные данные: ");
+	System.out.println("a равно: " + a);
+	System.out.println("b равно :" + b);
+	System.out.println("c равно :" + c);
+	System.out.println("значение d = a*b/c = " + d);
+	}
+}
